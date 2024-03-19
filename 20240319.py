8b633507fea658f0b91ad6f0185eea8b5381e475
xodnd0507 <24457545yong@gmail.com>
2024-03-19 17:47:18 +0900

    test : Add 20240319 python

---
+++ 20240319.py
@@ -0,0 +1,38 @@
+# def solution(a, b):
+#     ab = int(str(a) + str(b))
+#     ba = int(str(b) + str(a))
+    
+#     if ab >= ba :
+#         return ab
+#     else :
+#         return ba
+# # 문자로 변환 후 합치고 숫자로 되돌려서 비교 후 출력
+
+# def solution(route):
+#     east = 0
+#     north = 0
+#     for i in route:
+#         if i == "N":
+#             north += 1
+#         elif i == "S" :    
+#             north -= 1
+#         elif i == "E" :
+#             east += 1
+
+#         elif i == "W":
+#             east -= 1
+
+#     return [east, north]
+
+def solution(arr):
+    for i in range(len(arr)):
+        print(str(arr[i]), end="")
+
+        arr = ["1", "2", "3"]
+        solution(arr)
+    # print(arr[0], end="") 
+    # print(arr[1], end="")
+    # print(arr[2], end="")
+    
+    # answer = 0
+    # return answer
