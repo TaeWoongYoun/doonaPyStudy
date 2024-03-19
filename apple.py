8d39b331b88b14f0e83152c9877d50e9481112f1
xodnd0507 <24457545yong@gmail.com>
2024-03-19 10:29:23 +0900

    test : Test 2dan

--- apple.py
+++ apple.py
@@ -1,8 +1,9 @@
-재고량 = 10
+# 재고량 = 10
 
-print("Hello Python!")
+# if 재고량 > 0 :
+#     print("지금 주문 가능합니다.")
+# else :
+#     print("주문 불가")
 
-if 재고량 > 0 :
-    print("지금 주문 가능합니다.")
-else :
-    print("주문 불가")
+# for i in range(1,10) :
+#     print(i*2)
