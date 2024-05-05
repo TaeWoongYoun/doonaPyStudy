eeeb3f6331cab7a4541fd6d7e344d64bcf46b1fd
xodnd0507 <24457545yong@gmail.com>
2024-05-05 13:32:40 +0900

    fix : Test

--- 20240422.py
+++ 20240422.py
@@ -5,11 +5,3 @@
 #         answer.append(names[i])
 
 #     return answer
-
-def solution(n):
-    answer = 0
-    for i in range(1, n+1):
-        if n % i == 0:
-            answer += i
-    
-    return answer
