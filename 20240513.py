e12cc0fcbcd181c2edc6a5987312f743209f3139
xodnd0507 <24457545yong@gmail.com>
2024-05-15 22:58:48 +0900

    test : Tess py

--- 20240513.py
+++ 20240513.py
@@ -22,3 +22,36 @@ def solution(x):
         answer = True
     
     return answer
+
+# def solution(dots):
+# [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]= dots
+# answer1 = ((y1-y2)*(x3-x4) == (y3-y4)*(x1-x2))
+# answer2 = ((y1-y3)*(x2-x4) == (y2-y4)*(x1-x3))
+# answer3 = ((y1-y4)*(x2-x3) == (y2-y3)*(x1-x4))
+# return 1 if answer1 or answer2 or answer3 else 0
+
+# from itertools import combinations
+
+# def solution(dots):
+#     new_dots = [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]
+#     com = list(combinations(new_dots, 2))
+
+# def solution(wall):
+#     a, b = [], []
+#     for i in range(len(wall)):
+#         for j in range(len(wall[i])):
+#             if wall[i][j] == "#":
+#                 a.append(i)
+#                 b.append(j)
+#     return [min(a), min(b), max(a) + 1, max(b) + 1]
+
+def solution(wallpaper):
+    x, y = set(), set()
+
+    for i in range(len(wallpaper)):
+        for j in range(len(wallpaper[i])):
+            if wallpaper[i][j] == "#":
+                x.add(i)
+                y.add(j)
+                
+    return [min(x), min(y), max(x) + 1, max(y) + 1]
